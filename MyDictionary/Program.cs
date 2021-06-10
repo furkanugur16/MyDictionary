9f410c3641113d7f73b90001699aba8940ac6c1b
furkan uğur <furkanugur16@outlook.com>
2021-06-10 19:17:25 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> games = new MyDictionary<int, string>();
+
+            games.Add(1 ,"Spider-Man");
+            games.Add(2, "Monster Hunter World");
+            games.Add(3, "Fifa2021");
+            games.Add(4, "Resident Evil 8");
+            games.Add(5, "Call of Duty");
+
+
+            Console.WriteLine("All games you have played: " + games.Count);
+
+            foreach (var game in games.Values)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
